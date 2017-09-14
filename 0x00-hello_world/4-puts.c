2ec9b7ed3729996801bbffae20a4397003bc0c67
vkalia602 <267@holbertonschool.com>
2017-09-14 01:53:04 +0000

    updated Printing sentence with a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-
-int main (void)
-
+/*
+ *printing sentence with a new line
+*/
+int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle");
-	return 0;
-
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
