f2ae62558cff3ce8b3856f615f4148f96877fbfd
vkalia602 <267@holbertonschool.com>
2017-09-15 00:08:34 +0000

    Alphabet loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -Print the sentence
+ * Return: print a 0 value
+ */
+
+int main(void)
+{
+	char al;
+
+	for (al = 'a' ; al <= 'z' ; al++)
+	{
+		putchar(al);
+	}
+	putchar('\n');
+	return (0);
+}
