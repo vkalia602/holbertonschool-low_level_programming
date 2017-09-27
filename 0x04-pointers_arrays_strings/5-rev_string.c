5e02ca27eab2659c7849437f5b4ac10bd2d2ce2b
vkalia602 <267@holbertonschool.com>
2017-09-27 05:29:16 +0000

    updated q 5

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -4,12 +4,24 @@
  * @s: pointer that points to s
  * Return: void
  */
+int _strlen(char *st)
+{
+        int l = 0;
+
+        while (*st)
+        {
+                l++;
+                st++;
+        }
+        return (l);
+}
+
 void rev_string(char *s)
 {
 	char t[10];
 	int x, y;
 
-	x = 8;
+	x = (_strlen(s) - 1);
 	y = 0;
 
 	while (s[y])
