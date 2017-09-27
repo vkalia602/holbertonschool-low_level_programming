fe6c563c6c3ae8365778456b6cbefc1e3bce07ed
vkalia602 <267@holbertonschool.com>
2017-09-27 04:25:11 +0000

    function that takes a pointer to a specified int

---
+++ 0x04-pointer_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - a function to takes a pointer to an int as parameter
+ * @n: pointer to variable
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
