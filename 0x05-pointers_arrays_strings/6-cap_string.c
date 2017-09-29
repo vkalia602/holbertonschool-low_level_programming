1c7d8c8b12ba9d2082dcdd291c6125dfe82f2e9d
vkalia602 <267@holbertonschool.com>
2017-09-29 06:59:16 +0000

    q6

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ *cap_string -that capitalizes all words of a string
+ * @s: string to be evaluated
+ *
+ * Return: s.
+ */
+char *cap_string(char *s)
+{
+	char sc[] = {32, 9, 10, 44, 59, 46, 33, 43, 34, 40, 41, 123, 125};
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			j = 0;
+			while (sc[j])
+			{
+				if (sc[j] == s[i - 1])
+				{
+					s[i] -= 32;
+					break;
+				}
+				j++;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
