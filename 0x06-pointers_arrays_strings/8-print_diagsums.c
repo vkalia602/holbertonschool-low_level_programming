9c5bb94d2629cf63a66b3cfff0deed4e39d6d0e3
vkalia602 <267@holbertonschool.com>
2017-10-03 05:26:08 +0000

    fixed printf

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -8,27 +8,26 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, sum, temp;
+	int i, sum1, temp, sum2;
 
 	i = 0;
-	sum = 0;
+	sum1 = 0;
 	temp = 0;
 	while (i <= (size * size))
 	{
 		temp = a[i];
-		sum = sum + temp;
+		sum1 = sum1 + temp;
 		i += (size + 1);
 	}
-	printf("%d, ", sum);
 
 	i = (size * size) - 1;
-	sum = 0;
+	sum2 = 0;
 	temp = 0;
 	while (i > 0)
 	{
 		i -= (size - 1);
 		temp = a[i];
-		sum = sum + temp;
+		sum2 = sum2 + temp;
 	}
-	printf("%d\n", sum);
+	printf("%d, %d\n", sum1, sum2);
 }
