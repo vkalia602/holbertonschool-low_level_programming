c46d265fab528a1f7e692ad406b2883308e86b8d
vkalia602 <267@holbertonschool.com>
2017-10-03 18:49:57 +0000

    fixed 2nd loop

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -8,26 +8,22 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, sum1, temp, sum2;
+	int i, sum1, sum2;
 
 	i = 0;
 	sum1 = 0;
-	temp = 0;
 	while (i <= (size * size))
 	{
-		temp = a[i];
-		sum1 = sum1 + temp;
+		sum1 = sum1 + a[i];
 		i += (size + 1);
 	}
 
 	i = (size * size) - 1;
 	sum2 = 0;
-	temp = 0;
 	while (i > 0)
 	{
 		i -= (size - 1);
-		temp = a[i];
-		sum2 = sum2 + temp;
+		sum2 = sum2 + a[i];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
