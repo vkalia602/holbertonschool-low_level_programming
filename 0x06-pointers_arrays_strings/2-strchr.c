78584bcd05f342720e0737ded15f848bc69042d7
vkalia602 <267@holbertonschool.com>
2017-10-03 05:51:12 +0000

    fixed condition statement

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,13 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i] != '\0' && s[i] != c)
 	{
-		if (s[i] == c)
-		{
-			return (s);
-		}
 		s++;
+	}
+	if (s[i] == c)
+	{
+		return (s);
 	}
 	return (NULL);
 }
