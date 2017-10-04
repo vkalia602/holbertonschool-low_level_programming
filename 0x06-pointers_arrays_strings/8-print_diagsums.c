fbcb779378ab567058b4b294a3fd8663dae65393
vkalia602 <267@holbertonschool.com>
2017-10-04 00:04:01 +0000

    fixed in 2nd loop

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -1,29 +1,37 @@
 #include "holberton.h"
 #include <stdio.h>
-/**
- * print_diagsums - prints the sum of two diagonals of a square matrix
- * @a: pointer to the array
- * @size: size of the array
- * Return: void
- */
-void print_diagsums(int *a, unsigned int size)
+
+
+void print_diagsums(int *a, int size)
 {
-	unsigned int i, sum1, sum2;
+	int i, sum1, sum2;
 
 	i = 0;
 	sum1 = 0;
 	while (i < (size * size))
 	{
+#ifdef __DEBUG
+		printf("B i = %d - a[i] = %d -  sum1 = %d\n", i, a[i], sum1);
+#endif
 		sum1 = sum1 + a[i];
 		i += (size + 1);
+#ifdef __DEBUG
+		printf("E i = %d -  sum1 = %d\n", i, sum1);
+#endif
 	}
 
 	i = (size * size) - 1;
 	sum2 = 0;
-	while (i > 0)
+	while (i > size)
 	{
 		i -= (size - 1);
+#ifdef __DEBUG
+		printf("M i = %d - a[i] = %d -  sum2 = %d\n", i, a[i], sum2);
+#endif
 		sum2 = sum2 + a[i];
+#ifdef __DEBUG
+		printf("E i = %d -  sum2 = %d\n", i, sum2);
+#endif
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
