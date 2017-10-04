e5cfb87aacaeb488287e7f5780cd107cc370fb7c
vkalia602 <267@holbertonschool.com>
2017-10-04 21:54:03 +0000

    Prints a string using recursion

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar ('\n');
+		return;
+	}
+	_putchar (*s);
+	_puts_recursion(s + 1);
+}
