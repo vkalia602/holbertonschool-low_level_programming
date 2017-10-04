1c1919257f63096d89146a6c45f10ee5e667f0ec
vkalia602 <267@holbertonschool.com>
2017-10-04 22:31:28 +0000

    Power function

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - Function returns the value of x raised to power of y.
+ * @x: number
+ * @y: exponent value
+ * Return: x raised to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (x * (_pow_recursion(x, (y - 1))));
+}
