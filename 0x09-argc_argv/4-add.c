fe838df0b01a506bef02184560a789c28d55e25a
vkalia602 <267@holbertonschool.com>
2017-10-08 21:49:10 +0000

    fixed test case

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -21,22 +21,25 @@ int _isdigit(int c)
  */
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, j, sum;
 
 	sum = 0;
-	if (argc < 1)
+	if (argc < 2)
+	{
 		printf("0\n");
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
-		if (_isdigit(*argv[i]))
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			sum += atoi(argv[i]);
-		}
-		else
-		{
+			if (!_isdigit(argv[i][j]))
+			{
 			printf("Error\n");
 			return (1);
+			}
 		}
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 		return (0);
