4e28a8a7f42f3084f6222fb7c271d36e80bcaf68
vkalia602 <267@holbertonschool.com>
2017-10-08 01:30:43 +0000

    Fixed comments and formatting

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main(int argc, char* argv[])
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: argument count
+ * @argv: character string ofthe argument
+ * Return: 0
+ */
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - 1]);
 	return (0);
