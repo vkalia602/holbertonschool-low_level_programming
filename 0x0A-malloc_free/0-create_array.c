bb1308b3cdccbc70410d758d6f9bef6f554fe9ef
vkalia602 <267@holbertonschool.com>
2017-10-10 21:40:11 +0000

    fixed statement for malloc fail

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -13,7 +13,9 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	p = malloc(sizeof(*p) * size);
+	p = malloc(sizeof(char) * size);
+	if (p == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
