0db0578ae8c6489a53432414bd19f7e2d140f68f
vkalia602 <267@holbertonschool.com>
2017-10-12 23:14:18 +0000

    fixed betty formats

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
 #include <stdlib.h>
-
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of malloc
+ * Return: pointer p with the allocated memory
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *p;
 
-	p = malloc (b);
+	p = malloc(b);
 	if (p == NULL)
 		exit(98);
 	return (p);
