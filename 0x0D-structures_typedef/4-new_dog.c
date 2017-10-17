6dd7c6dbb147561fa33490a5f94f4769f6bbf6da
vkalia602 <267@holbertonschool.com>
2017-10-17 04:26:01 +0000

    First commit - fucntion creates a new dog

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,82 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strlen - returns a length of the string
+ * @s: string to be evaluated
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s)
+	{
+		l++;
+		s++;
+	}
+	return (l);
+}
+/**
+ * *_strcpy - function that copies the string pointed to by src to dest
+ * @src:  string to be copied
+ * @dest: returns the copied string
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+ * *new_dog - Creates a new dog
+ * @name: first member of the struct- name of the new dog
+ * @age: second member of the struct - age of the new dog
+ * @owner: third member of the struct - name of the owner
+ * Return: pointer that points to new dog struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	char *copyname, *copyowner;
+	dog_t *ptr;
+
+	if (name != NULL && owner != NULL)
+	{
+		ptr = malloc(sizeof(dog_t));
+		if (ptr == NULL)
+		{
+			free(ptr);
+			return (NULL);
+		}
+		copyname = malloc(_strlen(name) * sizeof(char));
+		if (copyname == NULL)
+		{
+			free(copyname);
+			return (NULL);
+		}
+		copyowner = malloc(_strlen(owner) * sizeof(char));
+		if (copyowner == NULL)
+		{
+			free(copyowner);
+			return (NULL);
+		}
+		_strcpy(copyname, name);
+		_strcpy(copyowner, owner);
+
+		ptr->name = copyname;
+		ptr->age = age;
+		ptr->owner = copyowner;
+		return (ptr);
+	}
+	else
+		return (NULL);
+
+
+}
