2ccb110d4e0e8997b00b56b726d5a05e0fabc2bb
vkalia602 <267@holbertonschool.com>
2017-10-17 00:30:21 +0000

    First commit- Created a struct named dog

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H_
+#define DOG_H_
+/**
+ * struct dog - struct for dog information
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Description: struct dog contains information of a dog like the name, age
+ * and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
