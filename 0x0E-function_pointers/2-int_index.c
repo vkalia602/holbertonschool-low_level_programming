c153c1013a5b4eb3a7e6a84bdf030b109ca97a1d
vkalia602 <267@holbertonschool.com>
2017-10-19 01:14:51 +0000

    Fixed return value when checking for null pointer

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -13,9 +13,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array == NULL)
-		return;
+		return (-1);
 	if (cmp == NULL)
-		return;
+		return (-1);
 	if (size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
