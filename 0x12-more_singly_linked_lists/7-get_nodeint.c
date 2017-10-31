9608f43ff46a3aeecf7fa60afeecc4d112460b89
vkalia602 <267@holbertonschool.com>
2017-10-31 19:55:23 +0000

    fixed while loop to iterate to less than idx

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 	ptr = head;
-	while (i <= index)
+	while (i < index)
 	{
 		if (ptr == NULL)
 			return (NULL);
