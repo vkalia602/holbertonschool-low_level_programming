ebad87f2e0364ccebb5a8aa11c6c0b3c056b1f64
vkalia602 <267@holbertonschool.com>
2017-11-01 00:30:30 +0000

    fixed null statement for next link

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	while (i < (index - 1) && ptr != NULL)
 	{
-		if (ptr == NULL)
+		if (ptr == NULL || ptr->next == NULL)
 			return (-1);
 		ptr = ptr->next;
 		i++;
