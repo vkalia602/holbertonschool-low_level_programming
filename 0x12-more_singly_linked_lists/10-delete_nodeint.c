d8c2670945b6adf63e03103854da56c65183b6c9
vkalia602 <267@holbertonschool.com>
2017-11-01 00:27:21 +0000

    fixed free order

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,8 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	d_ptr = ptr->next;
 	temp = d_ptr->next;
-	ptr->next = temp;
-
 	free(d_ptr);
+	ptr->next = temp;
 	return (1);
 }
