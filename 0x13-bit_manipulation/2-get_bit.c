244b9d5ea4dc8790521c00b12f02c294f08162f0
vkalia602 <267@holbertonschool.com>
2017-11-03 22:16:37 +0000

    First commit

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * get_bit - Function that returns the value of a bit at a given index.
+ * @n: Number
+ * @index: index of the bit to be returned
+ * Return: Value of the bit at that index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int x = 0;
+
+	x = n >> index;
+	if (x & 1)
+		return (1);
+	else
+		return (0);
+}
