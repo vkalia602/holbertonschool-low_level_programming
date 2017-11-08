bf4714a828c183f2b9524b0e84a32e94bcb14f35
vkalia602 <267@holbertonschool.com>
2017-11-08 03:49:57 +0000

    First Commit

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "holberton.h"
+/**
+ * read_textfile - Function that reads a text file and
+ * prints it to standard output.
+ * @filename: Text file
+ * @letters: number of letters to read or print
+ * Return: Count of the letters printed.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	int count;
+	char *buffer;
+
+	buffer = malloc(letters * sizeof(char) + 1);
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	count = read(fd, buffer, letters);
+	buffer[count] = '\0';
+	close(fd);
+
+	fd = write(1, buffer, count);
+	if (fd == -1)
+		return (0);
+
+	free(buffer);
+	return (count);
+
+}
---
+++ 0x14-file_io/README.md
@@ -0,0 +1,5 @@
+0x14. C- file_io
+0-read_textfile.c - Function that reads a text file and prints it to the POSIX standard output.
+1-create_file.c - Function that creates a file.
+2-append_text_to_file.c - Function that appends a text at the end of a file.
+3-cp.c - Program that copies the content of a file to another file.
