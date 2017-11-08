30f07d3342c952456f096f7903981d594e241f6f
vkalia602 <267@holbertonschool.com>
2017-11-08 05:35:03 +0000

    fixed error statements

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -17,22 +17,34 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content != NULL)
 	{
 		length = strlen(text_content);
-		fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 		if (fd == -1)
+		{
+			close(fd);
 			return (-1);
+		}
 		w_len = write(fd, text_content, length);
-		if (w_len == -1)
+		if (w_len < 0)
+		{
+			close(fd);
 			return (-1);
+		}
 
 	}
 	else
-		fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+	{
+		fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+		if (fd == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
 	close(fd);
-	return (0);
-
-
+	return (1);
 }
