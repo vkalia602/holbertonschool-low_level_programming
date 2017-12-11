7b711427dd79fce472e995e1e143567cd51b6b39
vkalia602 <267@holbertonschool.com>
2017-12-11 02:07:17 +0000

    fixed new_node null check

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -15,8 +15,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	new_node->next = *head;
-	*head = new_node;
 	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
+	if (new_node->next != NULL)
+		new_node->next->prev = new_node;
 	return (new_node);
 }
