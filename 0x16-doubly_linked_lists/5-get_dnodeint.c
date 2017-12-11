c418de86d344dc21283d70202bc067ef3bd71d7d
vkalia602 <267@holbertonschool.com>
2017-12-11 01:11:47 +0000

    Fixed NULL check

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -11,6 +11,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *ptr;
 	unsigned int i = 0;
 
+	if (head == NULL)
+		return (NULL);
 	ptr = head;
 	while (i < index)
 	{
