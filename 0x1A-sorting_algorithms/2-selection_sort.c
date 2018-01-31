e5078fc34294783ae0d9b6a728ea7723c51c5e87
vkalia602 <vkalia602@gmail.com>
2018-01-31 08:02:37 +0000

    Added size check for the array

--- 0x1A-sorting_algorithms/2-selection_sort.c
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -1,20 +1,36 @@
 #include "sort.h"
-
+/**
+ * selection_sort - function that sorts an array of integers
+ * in ascending order using the Selection sort algorithm.
+ *
+ * @array: array of integers
+ * @size: Size of the array
+ *
+ * Return: void
+ */
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, x;
-	int num;
+	int compare;
 
+	if (size < 2)
+		return;
 	for (i = 0; i < size; i++)
 	{
+		compare = array[i];
 		for (j = i; j < size; j++)
 		{
-			if (array[i] > array[j])
+			if (array[j] < compare)
+			{
 				x = j;
+				compare = array[j];
+			}
 		}
-		num = array[i];
-		array[i] = array[x];
-		array[x] = num;
-		print_array(array, size);
+		if (compare != array[i])
+		{
+			array[x] = array[i];
+			array[i] = compare;
+			print_array(array, size);
+		}
 	}
 }
