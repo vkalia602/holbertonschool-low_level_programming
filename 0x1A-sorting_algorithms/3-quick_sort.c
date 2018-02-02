17828f74bf015f462284bfe98f36f71042902fc1
vkalia602 <vkalia602@gmail.com>
2018-02-02 17:40:52 +0000

    Attempt at fixing loop

--- 0x1A-sorting_algorithms/3-quick_sort.c
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -1,13 +1,13 @@
 #include "sort.h"
 #include <stdio.h>
 
-size_t partition(int *array, size_t size, size_t high, size_t low)
+size_t partition(int *array, size_t size, int high, int low)
 {
-	size_t i;
+	int i, j;
 	int pivot, holder;
 
 	pivot = array[high];
-	printf("high- %lu, low- %lu\n", high, low);
+/*	printf("high- %d, low- %d\n", high, low);*/
 	for (i = low; i <= high; i++)
 	{
 		if (array[i] <= pivot)
@@ -19,23 +19,16 @@ size_t partition(int *array, size_t size, size_t high, size_t low)
 			low++;
 		}
 	}
-	if (array[high] < array[low])
-	{
-		holder = array[i];
-		array[i] = array[low];
-		array[low] = holder;
-		print_array(array, size);
-	}
 	return (low);
 }
-void recursive_sort(int *array, size_t size, size_t high, size_t low)
+void recursive_sort(int *array, size_t size, int high, int low)
 {
-	size_t parted_idx;
+	int parted_idx;
 
 	if (low < high)
 	{
 		parted_idx = partition(array, size, high, low);
-		printf("parted - %lu, x_high- %lu, x_low-%lu\n", parted_idx, high, low);
+/*		printf("parted - %d, x_high- %d, x_low-%d\n", parted_idx, high, low);*/
 		recursive_sort(array, size, parted_idx - 1, low);
 		recursive_sort(array, size, high, parted_idx + 1);
 	}
